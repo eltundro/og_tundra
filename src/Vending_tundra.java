ac6ba2626fa5a0126f668d7f79ece383e3b084ee
eltundro <ClaspingTundra@yandex.ru>
2023-01-30 17:27:58 +0400

    Написал вендинговый аппарат

--- src/Vending_tundra.java
+++ src/Vending_tundra.java
@@ -9,32 +9,32 @@ public class Vending_tundra {
         int Twix = 55;
         int MountainDew = 70;
 
-        System.out.println("\t\t\tВведите количество денег!");
+        System.out.println("\t\t\t  Введите количество денег:  ");
         int MoneyAmount = new Scanner(System.in).nextInt();
 
 
         boolean isCanBuy = false;
 
         if (MoneyAmount >=Suhariki) {
-            System.out.println("\t\t***Вы можете купить сухарики***");
+            System.out.println("\t\t*** Вы можете купить сухарики ***");
             isCanBuy = true;
         }
 
         if (MoneyAmount >=CocaCola) {
-            System.out.println("\t\t***Вы можете купить CocaCola***");
+            System.out.println("\t\t*** Вы можете купить CocaCola ***");
             isCanBuy = true;
         }
         if (MoneyAmount >=Mentos) {
-            System.out.println("\t\t***Вы можете купить Mentos***");
+            System.out.println("\t\t*** Вы можете купить Mentos ***");
             isCanBuy = true;
         }
         if (MoneyAmount >=Twix) {
-            System.out.println("\t\t***Вы можете купить Twix***");
+            System.out.println("\t\t*** Вы можете купить Twix ***");
             isCanBuy = true;
         }
         if (MoneyAmount >=MountainDew)
         {
-            System.out.println("\t\t***Вы можете купить MountDew***");
+            System.out.println("\t\t*** Вы можете купить MountDew ***");
             isCanBuy = true;
         }
         if (!isCanBuy) {
